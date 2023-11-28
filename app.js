afb1b90f6b9b25e46257049f2bdd4886b9f83d09
siddharthguptaa <sidg5455@gmail.com>
2023-11-28 20:41:14 +0100

    episode 3 final

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const parent = React.createElement("h1", {}, "Hi my first element");
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxheading = <h1 id="heading">Namaste React using JSX!</h1>;
+const first = <span>React Element</span>
+
+const JSXsxheading = () =>  (<h1 id="heading">{first}Namaste React using JSX!</h1>);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
-
+const number = 1000;
 const HeadingComponent = () => {
   return (
     <div id="container">
+        {JSXsxheading()}
+      <JSXsxheading />
+      <JSXsxheading></JSXsxheading>
       <h1 id="heading">My first functional component.</h1>
     </div>
   );
@@ -18,4 +22,4 @@ const HeadingComponent = () => {
 
 // const HeadingComponentOne = () => <h1 className="heading">My first functional component.</h1>;
 
-root.render(<HeadingComponent/>);
+root.render(<HeadingComponent />);
