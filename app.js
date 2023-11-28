f1e94820cd7248017da3afcbf573134a01ab73e3
Siddharth Gupta <Siddharth.Gupta@motherson.com>
2023-11-28 15:34:41 +0100

    changes from episode 3

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-
-
-const heading = React.createElement("h1", { id: "head" }, "Hello World from React!");
-
-
-const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" }, [React.createElement("h1", { id: "h1" }, "hello i'm h1 and i'm under child1"), React.createElement("h2", { id: "h2" }, "hello i'm h2 and i'm under child1")]), React.createElement("div", { id: "child2" }, [React.createElement("h1", { id: "h1" }, "hello i'm h1 and i'm under child2"), React.createElement("h2", { id: "h2" }, "hello i'm h2 and i'm under child2")])]);
-//console.log(parent);
-
+const jsxheading = <h1 id="heading">Namaste React using JSX!</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent); 
+const HeadingComponent = () => {
+  return (
+    <div id="container">
+      <h1 id="heading">My first functional component.</h1>
+    </div>
+  );
+};
+
+// const HeadingComponentOne = () => <h1 className="heading">My first functional component.</h1>;
+
+root.render(<HeadingComponent/>);
