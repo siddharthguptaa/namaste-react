502a8df59d528a56565b89860eb9ff541d97cee5
siddharthguptaa <sidg5455@gmail.com>
2023-11-28 16:40:04 +0100

    t

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
-
-
-
-const parent = React.createElement("h1",{},"Hi my first element");
+const parent = React.createElement("h1", {}, "Hi my first element");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
-
-
-git remote add origin https://github.com/OWNER/REPOSITORY.githttps://github.com/siddharthguptaa/namaste-react.git
